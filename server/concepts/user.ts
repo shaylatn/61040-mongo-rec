bd9acd785fed93a81c358290a18dfceef9f2720f
Shayla Nguyen <shaylatn@mit.edu>
2023-09-28 01:24:44 -0400

    prep3 not working

--- server/concepts/user.ts
+++ server/concepts/user.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
-import { BadValuesError, NotAllowedError, NotFoundError } from "./errors";
+import { BadValuesError, NotAllowedError } from "./errors";
 
 export interface UserDoc extends BaseDoc {
   username: string;
@@ -13,7 +13,7 @@ export default class UserConcept {
   async getById(_id: ObjectId) {
     // TODO 1: Implement this method
     // Hint: check out this.users.readOne
-    throw new Error("Not implemented!");
+    return await this.users.readOne(_id);
   }
 
   async create(username: string, password: string) {
@@ -25,7 +25,7 @@ export default class UserConcept {
   async update(_id: ObjectId, update: Partial<UserDoc>) {
     // TODO 2: Implement this method
     // Hint: check out this.users.updateOne
-    throw new Error("Not implemented!");
+    return await this.users.updateOne(_id, update);
   }
 
   // Sanitizes user object by removing password field
